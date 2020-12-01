3a6e323f2d360e8b9d7f91e17c5e74366352f76a
jsilance <juliensilance@gmail.com>
2020-12-01 23:46:02 +0100

    rm test

--- midi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-aseqdump -p "Launchpad" | \
-while IFS=" ," read src ev1 ev2 ch label1 data1 label2 data2 rest; do
-    case "$ev1 $ev2 $data1" in
-        # "Note on 0" ) xdotool type start http://google.com ;;
-        "Note on 0" ) xdotool command start http://google.com ;;
-        "Note on 48" ) xdotool key ctrl+j ;;
-    esac
-done
