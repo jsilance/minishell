a0f2333c2d9dadab42fcdc2e643c4ffba8b96169
jsilance <juliensilance@gmail.com>
2020-12-15 01:33:59 +0100

    automatic update

--- parser/main_parser.c
+++ parser/main_parser.c
@@ -6,7 +6,7 @@
 /*   By: jsilance <jsilance@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 01:53:26 by jsilance          #+#    #+#             */
-/*   Updated: 2020/12/09 03:41:39 by jsilance         ###   ########.fr       */
+/*   Updated: 2020/12/14 23:25:18 by jsilance         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,49 @@
 
 static int	command_chekcer(char *str)
 {
-	int	i;
-
-	if (str && !(i = 0))
+	if (str)
+	{
 		if (!ft_strcmp("exit", str))
-			return (127);
+			return (0);
+		else if (!ft_strcmp("echo", str))
+			return (1);
+		else if (!ft_strcmp("cd", str))
+			return (2);
+		else if (!ft_strcmp("pwd", str))
+			return (3);
+		else if (!ft_strcmp("export", str))
+			return (4);
+		else if (!ft_strcmp("env", str))
+			return (5);
+		else if (!ft_strcmp("help", str))
+			return (6);
+	}
 	// return (ft_error(1, -1));
-	return (1);
+	return (-1);
 }
 
 /*
-**	cmd_index 0 --> exit
-**	cmd_index 1 --> echo
-**	cmd_index 2 --> cd
-**	cmd_index 3 --> pwd
-**	cmd_index 4 --> export
-**	cmd_index 5 --> env
-**	cmd_index 6 --> help
+**	cmd_index -1 --> error
+**	cmd_index  0 --> exit
+**	cmd_index  1 --> echo
+**	cmd_index  2 --> cd
+**	cmd_index  3 --> pwd
+**	cmd_index  4 --> export
+**	cmd_index  5 --> env
+**	cmd_index  6 --> help
 */
 
 static void	chain_maker(t_sarg *t)
 {
 	t_list	*ptr;
+	int		index;
 
 	ptr = t->arg_lst;
+	index = -1;
 	while (ptr)
 	{
-		// if (!ft_strcmp("exit", ptr->content))
-			// ft_cmd_lstadd_back(t->cmd, ft_cmd_lstnew(ft_strdup(ptr->content), NULL, 0));
-		// else if (!ft_strcmp("echo", ptr->content))
-			// ft_cmd_lstadd_back(t->cmd, ft_cmd_lstnew(ft_strdup(ptr->content), NULL, 1));
-		// else if (!ft_strcmp("cd", ptr->content))
-			// ft_cmd_lstadd_back(t->cmd, ft_cmd_lstnew(ft_strdup(ptr->content), NULL, 2));
-		// else if (!ft_strcmp("pwd", ptr->content))
-			// ft_cmd_lstadd_back(t->cmd, ft_cmd_lstnew(ft_strdup(ptr->content), NULL, 3));
-		// else if (!ft_strcmp("export", ptr->content))
-			// ft_cmd_lstadd_back(t->cmd, ft_cmd_lstnew(ft_strdup(ptr->content), NULL, 4));
-		// else if (!ft_strcmp("env", ptr->content))
-			// ft_cmd_lstadd_back(t->cmd, ft_cmd_lstnew(ft_strdup(ptr->content), NULL, 5));
-		// else if (!ft_strcmp("help", ptr->content))
-			// ft_cmd_lstadd_back(t->cmd, ft_cmd_lstnew(ft_strdup(ptr->content), NULL, 6));
+		ft_cmd_lstadd_back(t->cmd, ft_cmd_lstnew(ft_strdup(ptr->content), NULL,
+			command_chekcer(ptr->content)));
 		ptr = ptr->next;
 	}
 }
